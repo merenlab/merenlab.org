59a0956a43c9de5962d4f5160a9a930bdbc99a04
Evan Kiefl <kiefl.evan@gmail.com>
2018-12-09 15:46:41 -0600

    f

--- data/2018_Delmont_et_al_SAR11_SAAVs/files/rename_all_in_ones.py
+++
@@ -1,9 +0,0 @@
-import os
-import glob
-
-all_in_ones = glob.glob("*.all_in_one")
-
-for all_in_one in all_in_ones:
-    gene_call = open(os.path.join(all_in_one, all_in_one.split(".")[0] + ".fasta"), "r").readline()
-    gene_call = gene_call.strip().replace(">","")
-    os.rename(all_in_one, gene_call + ".all_in_one")
